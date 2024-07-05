abd06666cd1f292f9b2451f666be8107b7e1ae37
MariussGustins <mariuss.gustins@outlook.com>
2024-07-05 10:01:24 +0300

    all done

--- Task2/Controllers/ApartmentsController.cs
+++ Task2/Controllers/ApartmentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Task2.Interface;
 using Task2.DTOs;
 
--- Task2/Controllers/HousesController.cs
+++ Task2/Controllers/HousesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Task2.Interface;
 using Task2.DTOs;
 using Task2.Services;
@@ -7,32 +8,36 @@ namespace Task2.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    // [Authorize]
     public class HousesController : ControllerBase
     {
         private readonly IHousesService _housesService;
         private readonly IApartmentService _apartmentService;
 
-        public HousesController(IHousesService housesService, IApartmentService apartmentService) 
+        public HousesController(IHousesService housesService, IApartmentService apartmentService)
         {
             _housesService = housesService;
-            _apartmentService = apartmentService; 
+            _apartmentService = apartmentService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<HouseDto>>> GetHouses()
+        // [Authorize(Policy = "ReadHouses")]
+        public async Task<IActionResult> GetHouses()
         {
             var houses = await _housesService.GetHousesAsync();
             return Ok(houses);
         }
-        [HttpGet("{id}/Apartments")] 
+
+        [HttpGet("{id}/Apartments")]
+        // [Authorize(Roles = "Resident,Manager")]
         public async Task<ActionResult<IEnumerable<ApartmentDto>>> GetApartmentsByHouseId(int id)
         {
             var apartments = await _apartmentService.GetApartmentsByHouseIdAsync(id);
             return Ok(apartments);
         }
 
-
         [HttpGet("{id}")]
+        // [Authorize(Roles = "Resident,Manager")]
         public async Task<ActionResult<HouseDto>> GetHouse(int id)
         {
             var house = await _housesService.GetHouseAsync(id);
@@ -46,6 +51,7 @@ namespace Task2.Controllers
         }
 
         [HttpPut("{id}")]
+        // [Authorize(Roles = "Manager")]
         public async Task<IActionResult> PutHouse(int id, HouseDto houseDto)
         {
             var result = await _housesService.UpdateHouseAsync(id, houseDto);
@@ -59,6 +65,7 @@ namespace Task2.Controllers
         }
 
         [HttpPost]
+        // [Authorize(Roles = "Manager")]
         public async Task<ActionResult<HouseDto>> PostHouse(HouseDto houseDto)
         {
             var houseId = await _housesService.CreateHouseAsync(houseDto);
@@ -66,6 +73,7 @@ namespace Task2.Controllers
         }
 
         [HttpDelete("{id}")]
+        // [Authorize(Roles = "Manager")]
         public async Task<IActionResult> DeleteHouse(int id)
         {
             var houseExists = await _housesService.HouseExistsAsync(id);
